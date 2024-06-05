57f7b95ae3f5e361e48db7dbf5e6a251c18e14b6
Parishi <117592741+ParishiB@users.noreply.github.com>
2024-06-05 17:36:36 +0530

    Update Contact.ts

--- controller/Contact.ts
+++ controller/Contact.ts
@@ -1,17 +1,39 @@
 import { Request, Response } from "express";
-import { PrismaClient } from "@prisma/client";
-import { createContactSchema, identifySchema } from "../utils/zod";
+import { PrismaClient, Contact } from "@prisma/client";
+import { identifySchema } from "../utils/zod";
+
 const prisma = new PrismaClient();
 
 export const identify = async (req: Request, res: Response) => {
   try {
-    const result = identifySchema.safeParse(req.body);
-    if (!result.success) {
-      return res.status(400).json(result.error.errors);
+    const { email, phoneNumber } = req.body;
+
+    // Zod for input validation
+
+    const parsedData = identifySchema.safeParse(req.body);
+
+    if (!parsedData.success) {
+      return res.status(400).json({ error: parsedData.error.errors });
     }
 
-    const { email, phoneNumber } = result.data;
-    if (!email && !phoneNumber) {
+    // **********************************************************************************************
+
+    const findEmail: any = await prisma.contact.findFirst({
+      where: {
+        email: email,
+      },
+    });
+    const findPhone: any = await prisma.contact.findFirst({
+      where: {
+        phoneNumber: phoneNumber,
+      },
+    });
+
+    // **********************************************************************************************
+
+    // CASE - 1 If none of the the phonenumber or email is not present in db, so a new row will be created
+
+    if (!findEmail && !findPhone) {
       const newContact = await prisma.contact.create({
         data: {
           phoneNumber: phoneNumber || null,
@@ -23,120 +45,226 @@ export const identify = async (req: Request, res: Response) => {
           deletedAt: null,
         },
       });
-      return res.status(400).json({
-        message:
-          "new contact is created since email and phoneNumber are not provided",
-      });
+      return res.status(200).json(newContact);
     }
+    // **********************************************************************************************
 
-    // Case II
-    const contacts = await prisma.contact.findMany({
-      where: {
-        OR: [
-          { email: email ?? undefined },
-          { phoneNumber: phoneNumber ?? undefined },
-        ],
-      },
-    });
+    //  CASE - 2 if either of the phone number or email is present so we create a new row and send thee response iwth all the primarycontactt
 
-    // Case III
+    // ***************CONDITION 1********************** //
 
-    const primaryContact = contacts[0];
-    const primaryContactId = primaryContact.id;
-    let emails: any = [];
-    let phoneNumbers: any = [];
-    let secondaryContactIds: any = [];
-
-    for (let i = 1; i < contacts.length; i++) {
-      await prisma.contact.update({
-        where: {
-          id: contacts[i].id,
-        },
-        data: {
-          linkedId: primaryContactId,
-          linkPrecedence: "secondary",
-          updatedAt: new Date(),
-        },
-      });
-      emails.push(contacts[i].email);
-      phoneNumbers.push(contacts[i].phoneNumber);
-      secondaryContactIds.push(contacts[i].id);
-    }
-
-    if (emails.length > 1 || phoneNumbers.length > 1) {
-      return res.status(200).json({
-        contact: {
-          primaryContactId: primaryContactId,
-          emails: emails,
-          phoneNumbers: phoneNumbers,
-          secondaryContactIds: secondaryContactIds,
-        },
-      });
-    }
-
-    if (contacts.length === 0) {
+    if (findEmail && !findPhone) {
       const newContact = await prisma.contact.create({
         data: {
-          email: email ?? undefined,
-          phoneNumber: phoneNumber ?? undefined,
-          linkPrecedence: "primary",
+          phoneNumber: phoneNumber || null,
+          email: email || null,
+          linkedId: findEmail.linkedId || findEmail.id,
+          linkPrecedence: "secondary",
           createdAt: new Date(),
           updatedAt: new Date(),
+          deletedAt: null,
         },
       });
 
-      return res
-        .status(201)
-        .json({ message: "New contact created", contact: newContact });
-    }
+      let phones: string[] = [];
+      let secondaryContacts: number[] = [];
 
-    // Case IV
-
-    let firstContactWithEmail = await prisma.contact.findFirst({
-      where: {
-        email: email,
-      },
-    });
-
-    let firstContactWithPhoneNumber = await prisma.contact.findFirst({
-      where: {
-        phoneNumber: phoneNumber,
-      },
-    });
-
-    let id1Value: any = firstContactWithEmail?.id;
-    let id2Value: any = firstContactWithPhoneNumber?.id;
-
-    if (id1Value !== undefined && id2Value !== undefined) {
-      if (id1Value > id2Value) {
-        await prisma.contact.update({
-          where: {
-            id: id2Value,
-          },
-          data: {
-            linkedId: id1Value,
-            linkPrecedence: "secondary",
-            updatedAt: new Date(),
-          },
-        });
-      } else {
-        await prisma.contact.update({
-          where: {
-            id: id1Value,
-          },
-          data: {
-            linkedId: id2Value,
-            linkPrecedence: "secondary",
-            updatedAt: new Date(),
-          },
-        });
-      }
-      return res.status(200).json({ message: "Case 4 satisfied" });
-    } else {
-      return res.status(400).json({
-        message: "Cannot update contacts. One or both IDs are undefined.",
+      const allContacts = await prisma.contact.findMany({
+        where: {
+          linkedId: findEmail.id,
+        },
       });
+
+      allContacts.forEach((contact) => {
+        if (contact.phoneNumber) {
+          phones.push(contact.phoneNumber);
+        }
+        if (contact.id) {
+          secondaryContacts.push(contact.id);
+        }
+      });
+
+      const contact = {
+        primaryContactId: findEmail.id,
+        email,
+        phoneNumbers: phones,
+        secondaryContactIds: secondaryContacts,
+      };
+
+      return res.status(200).json(contact);
     }
+    // ***************CONDITION 2********************** //
+
+    if (!findEmail && findPhone) {
+      const newContact = await prisma.contact.create({
+        data: {
+          phoneNumber: phoneNumber || null,
+          email: email || null,
+          linkedId: findPhone.linkedId || findPhone.id,
+          linkPrecedence: "secondary",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          deletedAt: null,
+        },
+      });
+
+      let emails: string[] = [];
+
+      let secondaryContacts: number[] = [];
+
+      const allContacts = await prisma.contact.findMany({
+        where: {
+          linkedId: findPhone.id,
+        },
+      });
+
+      allContacts.forEach((contact) => {
+        if (contact.email) {
+          emails.push(contact.email);
+        }
+        if (contact.id) {
+          secondaryContacts.push(contact.id);
+        }
+      });
+
+      const contact = {
+        primaryContactId: findPhone.id,
+        emails: emails,
+        phoneNumber,
+        secondaryContactIds: secondaryContacts,
+      };
+
+      return res.status(200).json(contact);
+    }
+    // **********************************************************************************************
+
+    // CASE - 3 if we want to change the primary contact to secondary contact
+
+    if (
+      findEmail.linkPrecedence === "primary" &&
+      findPhone.linkPrecedence === "primary"
+    ) {
+      if (findEmail.id > findPhone.id) {
+        const updatedContact = await prisma.contact.update({
+          where: {
+            id: findEmail.id,
+          },
+          data: {
+            linkPrecedence: "secondary",
+            linkedId: findPhone.id,
+          },
+        });
+
+        let emails: string[] = [];
+        let phones;
+        let secondaryContacts: number[] = [];
+
+        const allContacts = await prisma.contact.findMany({
+          where: {
+            linkedId: findPhone.id,
+          },
+        });
+
+        allContacts.forEach((contact) => {
+          if (contact.email) {
+            emails.push(contact.email);
+          }
+          if (contact.phoneNumber) {
+            phones = contact.phoneNumber;
+          }
+          if (contact.id) {
+            secondaryContacts.push(contact.id);
+          }
+        });
+
+        const contact = {
+          primaryContactId: findPhone.id,
+          emails: emails,
+          phoneNumber,
+          secondaryContactIds: secondaryContacts,
+        };
+
+        return res.status(200).json({ contact });
+      }
+
+      if (findEmail.id < findPhone.id) {
+        const updatedContact = await prisma.contact.update({
+          where: {
+            id: findPhone.id,
+          },
+          data: {
+            linkPrecedence: "secondary",
+            linkedId: findEmail.id,
+          },
+        });
+
+        let phones: string[] = [];
+        let secondaryContacts: number[] = [];
+
+        const allContacts = await prisma.contact.findMany({
+          where: {
+            linkedId: findEmail.id,
+          },
+        });
+
+        allContacts.forEach((contact) => {
+          if (contact.phoneNumber) {
+            phones.push(contact.phoneNumber);
+          }
+          if (contact.id) {
+            secondaryContacts.push(contact.id);
+          }
+        });
+
+        const contact = {
+          primaryContactId: findEmail.id,
+          email,
+          phoneNumbers: phones,
+          secondaryContactIds: secondaryContacts,
+        };
+
+        return res.status(200).json({ contact });
+      }
+    }
+
+    // CASE - 4 both the email and phoneNumber are present in the same row
+
+    const findContactId = await prisma.contact.findFirst({
+      where: {
+        AND: [{ email: email }, { phoneNumber: phoneNumber }],
+      },
+    });
+
+    let emails: string[] = [];
+    let phones: string[] = [];
+    let secondaryContacts: number[] = [];
+
+    if (findContactId) {
+      const contacts = await prisma.contact.findMany({
+        where: {
+          linkedId: findContactId.id,
+        },
+      });
+
+      contacts.forEach((contact) => {
+        if (contact.email) emails.push(contact.email);
+        if (contact.phoneNumber) phones.push(contact.phoneNumber);
+        secondaryContacts.push(contact.id);
+      });
+
+      const contactResponse = {
+        primaryContactId: findContactId.id,
+        emails,
+        phones,
+        secondaryContactIds: secondaryContacts,
+      };
+
+      return res.status(200).json({ contactResponse });
+    }
+
+    return res.status(404).json({ message: "Contact not found" });
+
+    // **********************************************************************************************
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Internal server error" });

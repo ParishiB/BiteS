9bccc4bd60fc7fcd5086770eecda99035e6f7f29
Parishi <117592741+ParishiB@users.noreply.github.com>
2024-06-05 17:36:57 +0530

    Delete routes directory

--- routes/Contact.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const router = express.Router();
-import { createContact, identify } from "../controller/Contact";
-
-router.post("/createContact", createContact);
-router.get("/identify", identify);
-
-module.exports = router;
